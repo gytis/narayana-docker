f10b9ae006e02b0562b8e1d6928a9f978756cac8
gytis <gytis@redhat.com>
2015-03-17 15:56:08 +0000

    Clean up environment variables and get back to port 4710

--- jts/transaction-service/docker-entrypoint.sh
+++ jts/transaction-service/docker-entrypoint.sh
@@ -2,16 +2,13 @@
 
 set -e
 
-export TRANSACTION_SERVICE_IP=$(grep "${HOSTNAME}" /etc/hosts | awk '{print $1}')
+export HOST_IP=$(grep "${HOSTNAME}" /etc/hosts | awk '{print $1}')
+[ -z "$NAME_SERVER_IP" ] && export NAME_SERVER_IP=$(grep 'name-server' /etc/hosts | awk '{print $1}')
+[ -z "$NAME_SERVER_URL" ] && export NAME_SERVER_URL="corbaloc::${NAME_SERVER_IP}:${NAME_SERVER_ENV_NAME_SERVER_PORT}/StandardNS/NameServer-POA/_root"
+[ -z "$NARAYANA_OPTS" ] && export NARAYANA_OPTS=""
 
-echo "Starting Narayana Transaction Service container on ${TRANSACTION_SERVICE_IP}:${NARAYANA_PORT}"
-
-# TODO use env variable to get containers name
-export NAME_SERVER_IP=$(grep 'name-server' /etc/hosts | awk '{print $1}')
-export NAME_SERVER_URL="corbaloc::${NAME_SERVER_IP}:${NAME_SERVER_ENV_NAME_SERVER_PORT}/StandardNS/NameServer-POA/_root"
-
-[ -z "$NARAYANA_OPTS" ] && NARAYANA_OPTS=""
-
+echo "Starting Narayana Transaction Service container on ${HOST_IP}:${NARAYANA_PORT}"
 echo "NARAYANA_OPTS=$NARAYANA_OPTS"
+echo "NAME_SERVER_URL=$NAME_SERVER_URL"
 
 exec "$@"
